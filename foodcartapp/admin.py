56be867d04bed2b1e9e06623c583612af73ab4b6
Norgius <dmitry.schadrin21@yandex.ru>
2023-01-27 23:03:18 +0300

    Small fixes

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.shortcuts import redirect
+from django.shortcuts import redirect, reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
 from django.utils.http import url_has_allowed_host_and_scheme
@@ -49,7 +49,8 @@ class ProductAdmin(admin.ModelAdmin):
         'category',
     ]
     search_fields = [
-        # FIXME SQLite can not convert letter case for cyrillic words properly, so search will be buggy.
+        # FIXME SQLite can not convert letter case for
+        # cyrillic words properly, so search will be buggy.
         # Migration to PostgreSQL is necessary
         'name',
         'category__name',
@@ -93,14 +94,18 @@ class ProductAdmin(admin.ModelAdmin):
     def get_image_preview(self, obj):
         if not obj.image:
             return 'выберите картинку'
-        return format_html('<img src="{url}" style="max-height: 200px;"/>', url=obj.image.url)
+        return format_html('<img src="{url}" style="max-height: 200px;"/>',
+                           url=obj.image.url)
     get_image_preview.short_description = 'превью'
 
     def get_image_list_preview(self, obj):
         if not obj.image or not obj.id:
             return 'нет картинки'
         edit_url = reverse('admin:foodcartapp_product_change', args=(obj.id,))
-        return format_html('<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>', edit_url=edit_url, src=obj.image.url)
+        return format_html(
+            '<a href="{edit_url}"><img src="{src}" '
+            'style="max-height: 50px;"/></a>',
+            edit_url=edit_url, src=obj.image.url)
     get_image_list_preview.short_description = 'превью'
 
 
