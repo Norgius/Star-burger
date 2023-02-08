30782617932b8db237bddbd9a83896ddc8905707
Norgius <dmitry.schadrin21@yandex.ru>
2023-02-08 23:07:56 +0300

    Fix static for admin

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -18,10 +18,22 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
+from django.conf.urls import url
+from django.views.static import serve
 
 from . import settings
 
 urlpatterns = [
+    url(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT},
+    ),
+    url(
+        r'^static/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.STATIC_ROOT},
+    ),
     path('admin/', admin.site.urls),
     path('', render, kwargs={'template_name': 'index.html'}, name='start_page'),
     path('api/', include('foodcartapp.urls')),
