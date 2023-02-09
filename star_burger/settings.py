7ef1830f04c26112f19421f665f0f8b15d48dc0a
Norgius <dmitry.schadrin21@yandex.ru>
2023-02-09 21:25:51 +0300

    switch to PostgreSQL DB

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -91,9 +91,19 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+#     )
+# }
+
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+        default='postgres://{user}:{password}@localhost:5432/{name}'.format(
+            user=env.str('POSTGRES_DB_USER'),
+            password=env.str('POSTGRES_DB_PASSWORD'),
+            name=env.str('POSTGRES_DB_NAME')
+        )
     )
 }
 
