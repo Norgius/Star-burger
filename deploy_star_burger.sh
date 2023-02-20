584396ffa70126baf6debef4aeaf01e6fdf41a5c
Norgius <dmitry.schadrin21@yandex.ru>
2023-02-20 23:24:07 +0300

    Fix bash-script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-cd /opt/Star-burger || exit 1
-echo $(git pull) || exit 1
-echo $(. ./env/bin/activate) || exit 1
 
-echo $(pip install -r requirements.txt) || exit 1
-echo $(apt install -y nodejs) || exit 1
-echo $(apt install -y npm) || exit 1
-echo $(./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./") || exit 1
+set -e
+cd /opt/Star-burger
+git pull
+. ./env/bin/activate
 
-echo $(python manage.py collectstatic --noinput) || exit 1
-echo $(python manage.py migrate) || exit 1
-echo $(systemctl restart burger_backend.service) || exit 1
-echo $(systemctl reload nginx.service) || exit 1
+pip install -r requirements.txt
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+
+systemctl restart burger_backend.service
+systemctl reload nginx.service
 
 last_commit=$(git rev-parse HEAD)
-echo $(curl -H "X-Rollbar-Access-Token: 0b5d8ca7d05548969b1611c26e7a4767" \
-          -H "Content-Type: application/json" \
-          -X POST 'https://api.rollbar.com/api/1/deploy' \
-          -d '{"environment": "production", "revision": "'${last_commit}'", "rollbar_name": "starburger", "local_username": "Norgius", "status": "succeeded"}')
+curl -H "X-Rollbar-Access-Token: 0b5d8ca7d05548969b1611c26e7a4767" \
+     -H "Content-Type: application/json" \
+     -X POST 'https://api.rollbar.com/api/1/deploy' \
+     -d '{"environment": "production", "revision": "'${last_commit}'", "rollbar_name": "starburger", "local_username": "Norgius", "status": "succeeded"}'
 echo Deploy was successful
