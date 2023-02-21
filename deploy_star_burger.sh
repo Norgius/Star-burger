e2b674644d6f3da503f839f5a9f3d83c52402fb0
Norgius <dmitry.schadrin21@yandex.ru>
2023-02-21 20:05:46 +0300

    Now frontend requirements are always updated

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -6,6 +6,7 @@ git pull
 . ./env/bin/activate
 
 pip install -r requirements.txt
+npm ci --dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 
 python manage.py collectstatic --noinput
